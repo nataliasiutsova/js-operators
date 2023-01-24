933a6bc746203e83fcbca215ea8845af9cbd2ba9
nataliasiutsova <tashaklimenkova@gmail.com>
2023-01-25 00:56:46 +0300

    Changed task-score.js

--- task-score.js
+++ task-score.js
@@ -3,4 +3,4 @@ let scr2 = 30;
 let scr3 = 50;
 let studentName = 'Ivanov Ivan';
 let avr = Math.floor((scr1 + scr2 + scr3) / 3);
-console.log(`${studentName} average score ${avr}`);
+console.log(`${studentName.slice(0, 1)} average score ${avr}`);
