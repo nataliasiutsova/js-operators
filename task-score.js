45fa8dd754e8fa261efc7516a6129c0a83a61673
nataliasiutsova <113311223+nataliasiutsova@users.noreply.github.com>
2023-01-25 13:13:49 +0300

    Replaced Math.floor on the Math.round

--- task-score.js
+++ task-score.js
@@ -2,5 +2,5 @@ let scr1 = 20;
 let scr2 = 30;
 let scr3 = 50;
 let studentName = 'I.Ivanov';
-let avr = Math.floor((scr1 + scr2 + scr3) / 3);
+let avr = Math.round((scr1 + scr2 + scr3) / 3);
 console.log(`${studentName.slice(0, 1)} average score ${avr}`);
